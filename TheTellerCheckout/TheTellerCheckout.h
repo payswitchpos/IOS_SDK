82ae1fcf8b317efba5795e5f7b4c6cd0c1f8dab8
Augustine Addey <aaddey@payswitch.com.gh>
2020-11-04 18:19:45 +0000

    Initial Commit

---
+++ TheTellerCheckout/TheTellerCheckout.h
@@ -0,0 +1,19 @@
+//
+//  TheTellerCheckout.h
+//  TheTellerCheckout
+//
+//  Created by Addey Augustine on 04/11/2020.
+//  Copyright © 2020 TheTeller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheTellerCheckout.
+FOUNDATION_EXPORT double TheTellerCheckoutVersionNumber;
+
+//! Project version string for TheTellerCheckout.
+FOUNDATION_EXPORT const unsigned char TheTellerCheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheTellerCheckout/PublicHeader.h>
+
+
